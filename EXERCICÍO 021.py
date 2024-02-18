e2e2d7ac95b7f1c5a3ff7f21737fa7145c6d1f8b
Rodrigo Gois <142438530+RodrigoGois25@users.noreply.github.com>
2024-02-18 17:30:20 -0300

    Add files via upload

---
+++ EXERCICÍO 021.py
@@ -0,0 +1,6 @@
+import pygame
+pygame.init()
+pygame.mixer.music.load('musica_do_sherek.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
+print('Tocando agora a música Smash Mouth do All Star ')
